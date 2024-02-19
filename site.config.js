330bb9588fab4172ab6038b3ef2b25223bbac851
Juhwan-Yoon <160496661+Juhwan-Yoon@users.noreply.github.com>
2024-02-19 18:28:34 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    email: "yoonjuhwan0623@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
