fecc9ed8635469763316f080b701f7acaa783974
Juhwan-Yoon <160496661+Juhwan-Yoon@users.noreply.github.com>
2024-02-21 02:01:00 +0100

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -16,6 +16,24 @@ const NavBar: React.FC = () => {
   )
 }
 
+
+const NavBar: React.FC = () => {
+  const links = [{ id: 2, name: "Resume", to: "/resume" }]
+  return (
+    <StyledWrapper className="">
+      <ul>
+        {links.map((link) => (
+          <li key={link.id}>
+            <Link href={link.to}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </StyledWrapper>
+  )
+}
+
+
+
 export default NavBar
 
 const StyledWrapper = styled.div`
