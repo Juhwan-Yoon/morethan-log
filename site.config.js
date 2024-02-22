ac6e47a9370dd13641bcd892dcff6c42af48aa4d
Juhwan-Yoon <160496661+Juhwan-Yoon@users.noreply.github.com>
2024-02-22 16:06:09 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -55,7 +55,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "Juhwan_yoon/Comments",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
